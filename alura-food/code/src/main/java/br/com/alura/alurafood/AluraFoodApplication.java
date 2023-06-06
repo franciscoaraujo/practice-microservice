1b563c68931c7f70df8d2ef5818397f76d4ad09d
Francisco Araujo <francisco.araujo@flexpagamentos.com.br>
2023-06-06 11:11:47 -0300

    Commit inicial

---
+++ alura-food/code/src/main/java/br/com/alura/alurafood/AluraFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraFoodApplication.class, args);
+	}
+
+}
